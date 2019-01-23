0d682cb681dd3da73c8cdd42f11ab37da4544f75
Sarah York <abmaltly@gmail.com>
2019-01-23 20:04:26 +0800

    Working with inline styles
    * add ‘style’ object within render method.
    * easy to ensure it only applies to elements within the component

--- section_3/src/App.js
+++ section_3/src/App.js
@@ -35,12 +35,23 @@ class App extends Component {
   }
 
   render() {
+
+    const style = {
+      backGroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       // The below is actually JSX and not HTML.  It's just made to look like HTML
       <div className="App">
         <h1>Hi, i'm a React app</h1>
         <p>This is really working</p>
-        <button onClick ={() => this.switchNameHandler('Mandy')}> Switch Name</button>
+        <button 
+          style={style}
+          onClick ={() => this.switchNameHandler('Mandy')}> Switch Name</button>
         <Person 
           name={this.state.persons[0].name} 
           age={this.state.persons[0].age}/> {/* this refers to the class */}

fbb2acf2f2763e2fd88716b66363721d52eedf72
Sarah York <abmaltly@gmail.com>
2019-01-23 16:15:08 +0800

    JSX restrictions

--- section_3/src/App.js
+++ section_3/src/App.js
@@ -3,17 +3,14 @@ import './App.css';
 
 class App extends Component {
   render() {
-    // return (
-    //   // The below is actually JSX and not HTML.  It's just made to look like HTML
-    //   <div className="App">
-    //     <h1>Hi, i'm a React app</h1>
-    //   </div>
-    // );
-
-    // This equivalent to the above.
-    return React.createElement('div', {className: 'App'}, 
-      React.createElement('h1', null,  'Hi, i\'m a React App!!!')
-    )
+    return (
+      // The below is actually JSX and not HTML.  It's just made to look like HTML
+      <div className="App">
+        <h1>Hi, i'm a React app</h1>
+        <p>This is really working</p>
+      </div>
+      // everything needs to be inside one root element (e.g. this div)
+    );
   }
 }
 

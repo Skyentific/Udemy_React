6cccbeeee95151dac1bdd4be99324fc2ae916885
Sarah York <abmaltly@gmail.com>
2019-01-23 11:21:15 +0800

    Understanding JSX

--- section_3/src/App.js
+++ section_3/src/App.js
@@ -3,12 +3,17 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      // The below is actually JSX and not HTML.  It's just made to look like HTML
-      <div className="App">
-        <h1>Hi, i'm a React app</h1>
-      </div>
-    );
+    // return (
+    //   // The below is actually JSX and not HTML.  It's just made to look like HTML
+    //   <div className="App">
+    //     <h1>Hi, i'm a React app</h1>
+    //   </div>
+    // );
+
+    // This equivalent to the above.
+    return React.createElement('div', {className: 'App'}, 
+      React.createElement('h1', null,  'Hi, i\'m a React App!!!')
+    )
   }
 }
 

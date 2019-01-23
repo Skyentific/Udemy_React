e79966eb615f6e5fd7c42f3f1dc96ddc3e64dc86
Sarah York <abmaltly@gmail.com>
2019-01-23 17:01:17 +0800

    Understanding state
    * passing variables into the component JSX instead of hardcoding strings .  e.g. instead of name =“Sky”
    * in a class based components, define a ‘state’ property name.  Mot available in fuction components
    * state = {};
    * if ‘state’ changed, it will cause react to update the DOM

--- section_3/src/App.js
+++ section_3/src/App.js
@@ -3,15 +3,25 @@ import './App.css';
 import Person from './Person/Person' // should always use an upper case charater for customer compontents
 
 class App extends Component {
+
+  state = {
+    persons: [
+      {name: 'Sky', age: 48},
+      {name: 'Dids', age: 48},
+      {name: 'Test', age: 12}
+    ]
+  };
+
   render() {
     return (
       // The below is actually JSX and not HTML.  It's just made to look like HTML
       <div className="App">
         <h1>Hi, i'm a React app</h1>
         <p>This is really working</p>
-        <Person name = 'Sky' age = '48'/>
-        <Person name='Dids' age='48'/>
-        <Person name='Test' age='10'>My hobbies: racing</Person>
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/> {/* this refers to the class */}
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>My hobbies: racing</Person>
       </div>
       // everything needs to be inside one root element (e.g. this div)
     );

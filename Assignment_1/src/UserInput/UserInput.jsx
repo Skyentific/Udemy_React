61dc269dd76f8d99f94a56c1eb156664f7ad25d3
Sarah York <abmaltly@gmail.com>
2019-01-26 13:38:18 +0800

    Asignment 1 updated
    Added Inline style example into UserInput.jsx

--- Assignment_1/src/UserInput/UserInput.jsx
+++ Assignment_1/src/UserInput/UserInput.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 import './UserInput.css'
 
 const userInput = (props) => {
+
+    const inputStyle = {
+        border: '2px solid grey'
+    }
+
     return (
         <input className = 'UserInput'
+            style={inputStyle}
             type = "text" 
             onChange = {props.changed} 
             value = {props.name}

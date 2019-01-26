a8daba2a47225e577011acb18707828567789d24
Sarah York <abmaltly@gmail.com>
2019-01-26 13:07:33 +0800

    Assignment 1 completed

---
+++ Assignment_1/src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './App.css';
+import UserInput from './UserInput/UserInput';
+import UserOutput from './UserOutput/UserOutput';
+
+class App extends Component {
+  
+  state = {
+    username: [
+      {name: 'SKY'}
+    ]
+  }
+
+  userNameChangeHandler = (event) => {
+    this.setState({
+      username: [
+        {name: event.target.value}
+      ]
+    })
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <UserInput 
+          name={this.state.username[0].name}
+          changed ={this.userNameChangeHandler}/>
+    
+        <UserOutput name={this.state.username[0].name}></UserOutput>
+      </div>
+    );
+  }
+}
+
+export default App;

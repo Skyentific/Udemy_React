ffaa2aaf26e52404ddd60e7313c7662dd43d3498
Sarah York <abmaltly@gmail.com>
2019-01-26 13:59:35 +0800

    Section 4: Using conditionals to render content conditionally
    Enclose part of a render output (e.g. a div) in culry braces and you can use simple javascript (non-block syntax (e.g. ternary operations)) to conditionally render elements.

---
+++ section_4/src/App.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person' // should always use an upper case charater for customer compontents
+
+class App extends Component {
+
+  state = {
+    persons: [
+      {name: 'Sky', age: 48},
+      {name: 'Dids', age: 48},
+      {name: 'Test', age: 12}
+    ]
+  }
+
+  switchNameHandler = (newName) => {
+    // console.log('Was clicked');
+    // don't do this    this.state.persons[0].name = 'Sarah';
+    this.setState({
+      persons: [
+        {name: 'Sarah', age: 48},
+        {name: newName, age: 48},
+        {name: 'Who cares', age: 12}
+      ]
+    });
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState( {
+      persons: [
+        {name: 'Sarah', age: 48},
+        {name: event.target.value, age: 48},
+        {name: 'Who cares', age: 12}
+      ],
+      showPersons: false
+    });
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
+  }
+
+  render() {
+
+    const style = {
+      backGroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+    return (
+      // The below is actually JSX and not HTML.  It's just made to look like HTML
+      <div className="App">
+        <h1>Hi, i'm a React app</h1>
+        <p>This is really working</p>
+        
+        <button 
+          style={style}
+          onClick ={this.togglePersonsHandler}> Switch Name
+        </button>
+        
+        {/* 
+          Enclose jsx in curly braces and use simmple JS to return conditional resutls .
+          Simple JS includes ternary operations
+        */}
+        {
+          this.state.showPersons ?
+          <div>
+            <Person 
+              name={this.state.persons[0].name} 
+              age={this.state.persons[0].age}/> {/* this refers to the class */}
+            <Person 
+              name={this.state.persons[1].name} 
+              age={this.state.persons[1].age}
+              click={this.switchNameHandler.bind(this, 'Amanda')}
+              changed = {this.nameChangedHandler}/>
+            <Person 
+              name={this.state.persons[2].name} 
+              age={this.state.persons[2].age}>My hobbies: racing</Person>
+          </div> : null  // end of the ternary expression
+        }
+      
+      </div>
+      // everything needs to be inside one root element (e.g. this div)
+    );
+  }
+}
+
+export default App;

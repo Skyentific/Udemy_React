2b084e59376e78ed91c37ca34aacef0fae4b5611
Sarah York <abmaltly@gmail.com>
2019-01-28 16:28:50 +0800

    Burger builder: hide modal until order

--- the_burger_builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ the_burger_builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -21,7 +21,8 @@ class BurgerBuilder extends Component {
             meat: 0
         },
         totalPrice: 4,
-        purchasable: false
+        purchasable: false,
+        purchasing:  false
     }
 
     updatePurchaseState = (ingredients) => {
@@ -108,6 +109,10 @@ class BurgerBuilder extends Component {
         this.updatePurchaseState(updatedIngredients);
     }
 
+    purchaseHandler = () => {
+        this.setState({purchasing: true})
+    }
+
     render() {
         const disabledInfo = {
             ...this.state.ingredients
@@ -121,18 +126,20 @@ class BurgerBuilder extends Component {
         console.log(this.state.purchasable);
         return (
             <>
-                <Modal> 
+                
+                <Modal show ={this.state.purchasing}> 
                     <OrderSummary 
                         ingredients = {this.state.ingredients}/>
                 </Modal>
-                
+
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls
                     ingredientAdded = {this.addIngredientHandler}
                     ingredientRemoved ={ this.removeIngredientHandler}
                     disabled={disabledInfo}
                     purchasable = {this.state.purchasable}
-                    price = {this.state.totalPrice}/>
+                    price = {this.state.totalPrice}
+                    ordered = {this.purchaseHandler}/>
             </>
         )
     }

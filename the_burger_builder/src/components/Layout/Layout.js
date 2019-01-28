df325cb440ea969d8823377931e9d19c4f66686e
Sarah York <abmaltly@gmail.com>
2019-01-28 13:56:34 +0800

    Burger builder: Burger builder component added
    * added simple style for Layout

--- the_burger_builder/src/components/Layout/Layout.js
+++ the_burger_builder/src/components/Layout/Layout.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import styles from './Layout.module.css'
 
 const layout = (props) => (
     <>
         <div>Toolbar, Sidedrawer, Backdrop</div>
-        <main>
+        <main className={styles.Content}>
             {props.children}
         </main>
     </>
---
+++ the_burger_builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class BurgerBuilder extends Component {
+
+    render() {
+        return (
+            <>
+                <div>Burger (graphical)</div>
+                <div>Build controls</div>
+            </>
+        )
+    }
+}
+
+export default BurgerBuilder;

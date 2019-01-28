d70816f79e35482d3f7f468e5f96f1ca767b09f5
Sarah York <abmaltly@gmail.com>
2019-01-28 15:32:22 +0800

    Burger builder: Implemented removing ingredients

--- the_burger_builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ the_burger_builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -55,14 +55,56 @@ class BurgerBuilder extends Component {
 
     removeIngredientHandler = (type) => {
 
+        // get a count of how many of each ingredient is currently in the burger
+        const oldCount = this.state.ingredients[type];
+
+        // escape if there are no ingredients of that type
+        if (oldCount <= 0) {
+            return;
+        }
+        // ingrement the ingredient count
+        const updatedCount = oldCount - 1;
+        
+        // make a copy of the ingredients in the state
+        const updatedIngredients = {
+            ...this.state.ingredients
+        }
+        
+        // update the count of the relevant ingredient in the state
+        updatedIngredients[type] = updatedCount;
+
+        // work out the cost for the ingredient
+        const priceDeduction = INGREDIENT_PRICES[type];
+
+        // get get the old burger price
+        const oldPrice = this.state.totalPrice;
+        
+        // add the new ingredient price to the old price
+        const newPrice = oldPrice - priceDeduction;
+
+        // update the state
+        this.setState( {
+            totalPrice: newPrice,
+            ingredients: updatedIngredients
+        })
     }
 
     render() {
+        const disabledInfo = {
+            ...this.state.ingredients
+        };
+
+        for (let key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key] <= 0;
+        }
+        console.log(disabledInfo);
         return (
             <>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls
-                    ingredientAdded = {this.addIngredientHandler}/>
+                    ingredientAdded = {this.addIngredientHandler}
+                    ingredientRemoved ={ this.removeIngredientHandler}
+                    disabled={disabledInfo}/>
             </>
         )
     }

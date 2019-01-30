48f453116e8c16a36a472907483dbaf74bd62c43
Sarah York <abmaltly@gmail.com>
2019-01-30 09:13:32 +0800

    Http: Prevent reloading on routing
    * use ‘Link’
    import { Route, Link } from 'react-router-dom';
    
    <li><Link to="/">Home</Link></li>
           <li><Link to={{
                                    pathname: "/new-post",
                                    hash: '#submit',
                                    search: '?quick-submit=true'
                                }}>New Post</Link></li>

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import './Blog.css';
 import Posts from '../../containers/Blog/Posts/Posts';
+import NewPost from '../Blog/NewPost/NewPost';
 
 class Blog extends Component {
    
@@ -12,16 +13,18 @@ class Blog extends Component {
                 <header className="Blog">
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/new-post">New Post</a></li>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to={{
+                                pathname: "/new-post",
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }}>New Post</Link></li>
                         </ul>
                     </nav>
                 </header>
-                {/* <Route path="/" exact render ={() => <Posts/>}/> */}
                 <Route path="/" exact component={Posts} />
-                <Route path="/new-post" exact render ={() => <h1 style={{padding: "80px"}}>Home 2</h1>}/>
+                <Route path="/new-post" exact component={NewPost} />
 
-              {/* <Posts/> */}
             </div>
         );
     }

2123080f1c617d99d8b18e8decd2e74c318eacca
Sarah York <abmaltly@gmail.com>
2019-01-30 12:06:00 +0800

    Http: Switch
    limit Routes to only one route, not all routes that match the pattern.
    
    Order to routes is important so that one with paramers doesnt override another one.  e.g. /:id would interpret /new-post as id=new-post.

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 import './Blog.css';
 import Posts from '../../containers/Blog/Posts/Posts';
 import NewPost from '../Blog/NewPost/NewPost';
@@ -26,10 +26,11 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Route path="/" exact component={Posts} />
-                <Route path="/new-post" exact component={NewPost} />
-                <Route path="/:id" exact component={FullPost} />
-
+                <Switch>
+                    <Route path="/" exact component={Posts} />
+                    <Route path="/new-post" exact component={NewPost} />
+                    <Route path="/:id" exact component={FullPost} />
+                </Switch>
 
             </div>
         );

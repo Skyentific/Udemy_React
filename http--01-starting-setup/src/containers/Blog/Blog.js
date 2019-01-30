ac068ff44bb07df096c9a2b0e4727aa7667f93af
Sarah York <abmaltly@gmail.com>
2019-01-30 08:40:41 +0800

    Http: add routing
    * npm install —save react-router react-router-dom
    * import BrowserRouter in App.js and wrap the app in BrouserRouter component tags.

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
 import axios from '../../axios';
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
@@ -66,14 +63,6 @@ class Blog extends Component {
                 </header>
                 <section className="Posts">
                     {posts}
-                </section>
-                <section>
-                    <FullPost 
-                        id = {this.state.SelectedPostId} 
-                        />
-                </section>
-                <section>
-                    <NewPost />
                 </section>
             </div>
         );

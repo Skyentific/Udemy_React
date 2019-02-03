551a2ef066890ba52863749ead0d04aedf9f6bd2
Sarah York <abmaltly@gmail.com>
2019-02-03 11:52:13 +0800

    Burger builder: write confirmed order to firebase

--- the_burger_builder/src/containers/Checkout/ContactData/ContactData.js
+++ the_burger_builder/src/containers/Checkout/ContactData/ContactData.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Button from '../../../components/UI/Button/Button';
-import styles from './ContactData.module.css'
+import Spinner from '../../../components/UI/Spinner/Spinner';
+import styles from './ContactData.module.css';
+import axios from '../../../axios-orders';
 
 class ContactData extends Component {
     state = {
@@ -9,20 +11,66 @@ class ContactData extends Component {
         address: {
             street: '',
             postalCode: ''
-        }
+        },
+        loading: false
+    }
+
+    orderHandler = (event) => {
+        event.preventDefault();  // stop the 'Order" button from sending a request
+        console.log(this.props.ingredients);
+
+        this.setState({loading: true});
+
+        const order = {
+            ingredients: this.props.ingredients,
+            price: this.props.price,
+            customer: {
+                name: 'Sarah York',
+                address: {
+                    street: 'Test street',
+                    zipCode: '34345',
+                    country: 'Australia',
+                },
+                email: 'test@email.com'
+            },
+            deliveryMethod: 'fastest'
+        };
+
+        // firebase needs .json
+        axios.post('/orders.json', order)
+            .then(response => {
+                this.setState({
+                    loading: false});
+                this.props.history.push('/');
+            })
+            .catch(error => {
+                console.log(error);
+                this.setState({
+                    loading: false});
+            });
     }
 
     render () {
+        let form = (
+            <form>
+                <input className={styles.Input} type = "text" name="name" placeholder="Your Name"/>
+                <input className={styles.Input} type = "text" name="email" placeholder="Your Email"/>
+                <input className={styles.Input} type = "text" name="street" placeholder="Street"/>
+                <input className={styles.Input} type = "text" name="postal" placeholder="Postal Code"/>
+                <Button 
+                    btnType="Success"
+                    clicked={this.orderHandler}>Order</Button>
+            </form>
+        );
+        
+        if (this.state.loading) {
+            form = <Spinner />
+        }
+
         return (
             <div className = {styles.ContactData}>
                 <h4>Enter your contact Data</h4>
-                <form>
-                    <input className={styles.Input} type = "text" name="name" placeholder="Your Name"/>
-                    <input className={styles.Input} type = "text" name="email" placeholder="Your Email"/>
-                    <input className={styles.Input} type = "text" name="street" placeholder="Street"/>
-                    <input className={styles.Input} type = "text" name="postal" placeholder="Postal Code"/>
-                    <Button btnType="Success">Order</Button>
-                </form>
+                {form}
             </div>
         )
     }

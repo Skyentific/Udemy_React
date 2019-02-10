5f29e00458f0d5560e307ffd4b6d2c4456d4a895
Sarah York <abmaltly@gmail.com>
2019-02-10 12:34:31 +0800

    Burger builder: Reducer for Ingredients

--- the_burger_builder/src/store/reducer.js
+++ the_burger_builder/src/store/reducer.js
@@ -2,12 +2,39 @@ import * as actionTypes from './actions'
 
 // BurgerBuilder.js
 const initialState = {
-    ingredients: null,
+    ingredients: {
+        salad: 0,
+        bacon: 0,
+        cheese: 0,
+        meat: 0
+    },
     totalPrice: 4
 };
 
 const reducer = (state = initialState, action) => {
 
+    switch(action.type) {
+        case actionTypes.ADD_INGREDIENT:
+            return {
+                ...state,
+                ingredients: {
+                    ...state.ingredients,
+                    [action.ingredientName]: state.ingredients[action.ingredientName] + 1  // new ingredient name from action payload
+                }
+            };
+
+        case actionTypes.REMOVE_INGREDIENT:
+            return {
+                ...state,
+                ingredients: {
+                    ...state.ingredients,
+                    [action.ingredientName]: state.ingredients[action.ingredientName] - 1  // new ingredient name from action payload
+                }
+            };
+        
+        default:
+            return state;
+    }
 
 };
 

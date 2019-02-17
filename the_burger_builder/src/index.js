7714b84f1d7aa104537a4ff6ced5694059529590
Sarah York <abmaltly@gmail.com>
2019-02-17 14:48:07 +0800

    Burger builder:  installing Redux dev tools

--- the_burger_builder/src/index.js
+++ the_burger_builder/src/index.js
@@ -5,22 +5,13 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore } from 'redux';
 import rootReducer from './store/reducer';
 
-// middleware example
-const logger = store => {
-    return next => {
-        return action => {
-            console.log('[MiddleWare] Dispatching', action);
-            const result = next(action);
-            console.log('[Middleware] next state', store.getState());
-            return result;
-        }
-    }
-};
-
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const app = (
     <Provider store={store}>

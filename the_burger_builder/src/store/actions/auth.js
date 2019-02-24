eccab883624f64c98f3d18a6df90ae65598bf9cd
Sarah York <abmaltly@gmail.com>
2019-02-24 12:17:24 +0800

    Burger builder: getting a token from the backend

--- the_burger_builder/src/store/actions/auth.js
+++ the_burger_builder/src/store/actions/auth.js
@@ -1,4 +1,5 @@
 import * as actionTypes from './actionTypes';
+import axios from 'axios';
 
 export const authStart = () => {
     return {
@@ -23,5 +24,21 @@ export const auth = (email, password) => {
     return dispatch => {
         // authenticate user
         dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        };
+
+        axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyCtKa86bxinFB9TBd029J-5eg6J-0vETpw',
+            authData )
+            .then(response => {
+                console.log(response);
+                dispatch(authSuccess(response.data))
+            })
+            .catch(err => {
+                console.log(err);
+                dispatch(authFail(err));
+            });
     };
 };

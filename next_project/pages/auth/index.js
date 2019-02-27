45ee4fa4533cd845be82d06b608ead521b7243f7
Sarah York <abmaltly@gmail.com>
2019-02-27 13:22:55 +0800

    Next project

--- next_project/pages/auth/index.js
+++ next_project/pages/auth/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import User from '../../components/User';
+
 const indexPage = () => (
     <div>
         <h1>The Auth index page</h1>
+        <User 
+            name="SKY"
+            age={48}/>
     </div>
 );
 

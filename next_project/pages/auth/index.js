1e8a29615bfe6c5a9927ddf8000e4c31a03de447
Sarah York <abmaltly@gmail.com>
2019-02-27 13:44:53 +0800

    Next project: a special lifecycle hook
    getInitialProps

--- next_project/pages/auth/index.js
+++ next_project/pages/auth/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import User from '../../components/User';
 
-const indexPage = () => (
+const authIndexPage = (props) => (
     <div>
-        <h1>The Auth index page</h1>
+        <h1>The Auth index page - {props.appName}</h1>
         <User 
             name="SKY"
             age={48}/>
     </div>
 );
 
-export default indexPage
+authIndexPage.getInitialProps = (context) => {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            resolve({appName: 'Super App (Auth)'})
+        }, 1000);
+    });
+
+    return promise;
+}
+
+export default authIndexPage

3f3ee0249a44657bbb3f2a03e6bb16e2f5d69f2f
Sarah York <abmaltly@gmail.com>
2019-02-27 10:25:35 +0800

    Burger builder: how to test redux

---
+++ the_burger_builder/src/store/reducers/auth.test.js
@@ -0,0 +1,17 @@
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import reducer from './auth';
+import * as actionTypes from '../actions/actionTypes';
+
+describe('auth reducer', () => {
+    it('should return the initial state', () => {
+        expect(reducer(undefined, {})).toEqual({
+            token: null,
+            userId: null,
+            error: null,
+            loading: false,
+            authRedirectPath: '/'
+        })
+    })
+})
